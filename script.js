50eafd11900b0036a2fefaccd78fc4bc2de547dd
G-gold <oketolaolugbenga@gmail.com>
2022-12-04 12:21:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// console.log("hello");
+// const myObj = {
+//   name: "Sholly",
+//   scores: [50,30,40,60,90],
+//   country: "Nigeria",
+//   business: ["sewing", "investment", "Teaching", "sales"],
+//   relatives: ["Blessing", "Lizy", "Danny", "Tammy", "Ngozi"],
+
+// };
+// const [a,b,...c]=myObj.business
+// console.log(a,b,...c);
+// for (const biz of myObj.scores)
+// console.log();
+
+// // const []=myObj.scores
+// // console.log([]);
+
+// const myArr=myObj.scores
+// let sum =0;
+// for(const ans in myArr){
+// sum+=myArr[ans]}
+// console.log(sum);
+
+// const hourEl = document.querySelector(".hour");
+// const minuteEl = document.querySelector(".minute");
+// const secondEl = document.querySelector(".second");
+
+// const greet = () => {
+//   const myTime = new Date();
+//   hourEl.textContent = myTime.getHours();
+//   minuteEl.textContent = myTime.getMinutes();
+//   secondEl.textContent = myTime.getSeconds();
+// };
+// setInterval(greet, 1000);
+
+
+console.log(__dirname);
+console.log(__filename);
